4a0953dd9c8046514e3d5fca228efd700f192034
talatkuyuk <talatkuyuk@gmail.com>
2024-04-11 21:10:21 +0300

    fixed vite config

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,11 +3,10 @@ import { defineConfig } from "vite";
 /// <reference types="vitest" />
 export default defineConfig({
   test: {
-    include: ["tests/**/*.spec.ts"],
     coverage: {
       provider: "v8",
       reporter: [
-        ["lcov", { projectRoot: "./src" }],
+        ["lcov", { projectRoot: "./src" }], //
         ["json", { file: "coverage.json" }],
         "text",
       ],
